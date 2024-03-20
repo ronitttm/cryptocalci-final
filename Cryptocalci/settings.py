e43dac9b71ac13999b59b93d8b01e20aaef6c5ad
ronitttm <ronit.murpani@vit.edu.in>
2024-03-20 22:04:37 +0530

    vercel.json updated

--- Cryptocalci/settings.py
+++ Cryptocalci/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'Cryptocalci.urls'
